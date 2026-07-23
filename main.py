756fcd16216368c6d6a91e10e9c6775e14b170cc
Chukwuemeka Obinna Nwankwo <emeka.obinna971@gmail.com>
2026-07-24 00:26:42 +0100

    Implement quarantine layer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.utils.logger import get_logger
 from src.extract.extract_patients import extract_patient_data
 from src.validate.validation import validate_required_columns
 from src.validate.validation_runner import run_quality_checks
+from src.load.load_quarantine import write_quarantine
 
 
 logger = get_logger(__name__)
@@ -12,10 +13,23 @@ def main():
     validate_required_columns(patients)
     valid_df, invalid_records = run_quality_checks(patients)
     print("Valid records:", valid_df.count())
-    for invalid in invalid_records:
-         print("Invalid records:", invalid.count())
-    logger.info("Extraction successful.")
 
+    rule_names = [
+    "missing_patient_id",
+    "missing_hospital",
+    "future_admission_date",
+    "invalid_admission_type",
+    "duplicate_patient_id"]
+
+    for rule_name, invalid_df in zip(rule_names, invalid_records):
+
+        invalid_count = invalid_df.count()
+
+        if invalid_count > 0:
+
+            logger.info(f"{invalid_count} records failed {rule_name}")
+
+        write_quarantine(invalid_df,rule_name)
 
 if __name__ == "__main__":
     main()
---
+++ src/load/load_quarantine.py
@@ -0,0 +1,25 @@
+import os
+
+from src.utils.logger import get_logger
+from src.utils.config import load_config
+from pyspark.sql.functions import lit
+
+logger = get_logger(__name__)
+config = load_config()
+
+
+def write_quarantine(df, rule_name):
+
+    quarantine_path = os.path.join(config["storage"]["quarantine"],rule_name)
+
+    logger.info( f"Writing quarantine records to {quarantine_path}")
+
+    df = df.withColumn("failed_rule",lit(rule_name))
+
+    (
+        df.write
+        .mode("overwrite")
+        .parquet(quarantine_path)
+    )
+
+    logger.info("Quarantine write completed.")
